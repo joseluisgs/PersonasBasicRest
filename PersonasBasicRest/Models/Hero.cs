0f4de0ae2c2df97a3c28cd9fe14b5959b2132103
José Luis González <joseluisgonsan@gmail.com>
2024-09-16 10:07:27 +0200

    Modelo Hero

---
+++ PersonasBasicRest/Models/Hero.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonasBasicRest.Models;
+
+/**
+ * Representa un héroe en la aventura.
+ */
+public class Hero
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsBadGuy { get; set; }
+    
+}
