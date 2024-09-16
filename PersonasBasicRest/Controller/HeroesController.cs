09c91af1129cf8903740fb9abfb60bbdf504d0a3
José Luis González <joseluisgonsan@gmail.com>
2024-09-16 12:03:01 +0200

    Método put

--- PersonasBasicRest/Controller/HeroesController.cs
+++ PersonasBasicRest/Controller/HeroesController.cs
@@ -53,7 +53,11 @@ public class HeroesController : ControllerBase
 
         var hero = await _context.Heroes.FindAsync(id);
 
+        // Si es nulo héroe no existe, devuelve un 404 Not Found
         if (hero == null) return NotFound("Hero with this ID " + id + " not found.");
+
+        // Devuelve el héroe encontrado
+        _logger.Information($"Hero {hero.Id} retrieved");
         return Ok(hero.ToModel());
     }
 
@@ -63,10 +67,13 @@ public class HeroesController : ControllerBase
     [SwaggerResponse(201, "Hero created", typeof(Hero))]
     [SwaggerResponse(400, "Invalid hero name")]
     public async Task<ActionResult<Hero>> Post(
-        [FromBody] Hero hero
+        [FromBody] [SwaggerParameter("Hero to create", Required = true)]
+        Hero hero
     )
     {
         _logger.Debug("Creating a new hero");
+
+        // Valida el nombre del héroe
         if (string.IsNullOrWhiteSpace(hero.Name)) return BadRequest("Invalid hero name.");
 
         var entityToSave = hero.ToEntity();
@@ -75,6 +82,44 @@ public class HeroesController : ControllerBase
         await _context.Heroes.AddAsync(entityToSave); // Guarda el héroe en la base de datos
         await _context.SaveChangesAsync(); // Confirma los cambios en la base de datos
         _logger.Information($"Hero {entityToSave.Id} created");
+
+        // Devuelve el héro creado
         return CreatedAtAction(nameof(GetHero), new { id = entityToSave.Id }, entityToSave.ToModel());
+    }
+
+    // PUT: heroes/5
+    [HttpPut("{id:int}")]
+    [SwaggerOperation(Summary = "Update a hero", Description = "Updates a hero")]
+    [SwaggerResponse(200, "Hero updated", typeof(Hero))]
+    [SwaggerResponse(404, "Hero with this ID not found")]
+    [SwaggerResponse(400, "Invalid hero name")]
+    public async Task<ActionResult<Hero>> Put(
+        [SwaggerParameter("ID of the hero", Required = true)]
+        long id,
+        [FromBody] [SwaggerParameter("Hero to update", Required = true)]
+        Hero hero
+    )
+    {
+        _logger.Debug($"Updating hero with ID: {id}");
+
+        // Valida el nombre del héroe
+        if (string.IsNullOrWhiteSpace(hero.Name)) return BadRequest("Invalid hero name.");
+
+        // Busca el héroe en la base de datos, si no lo encuentra devuelve un 404 Not Found
+        var entityToUpdate = await _context.Heroes.FindAsync(id);
+        if (entityToUpdate == null) return NotFound("Hero with this ID " + id + " not found.");
+
+        // Copia los valores del héroe recibido al héroe en la base de datos
+        // No puedes usar directamente el héroe recibido con el mapper porque necesitamos la referencia del objeto
+        entityToUpdate.Name = hero.Name;
+        entityToUpdate.IsBad = hero.IsBad;
+
+        // Guarda los cambios en la base de datos
+        _context.Entry(entityToUpdate).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        _logger.Information($"Hero {entityToUpdate.Id} updated");
+
+        // Devuelve el héroe actualizado
+        return Ok(entityToUpdate.ToModel());
     }
 }
