f3c08956fc24dafc2f7d668d4e5fc46913c5a5e4
José Luis González <joseluisgonsan@gmail.com>
2024-09-16 10:32:14 +0200

    Cargando configuración de Programa

--- PersonasBasicRest/Program.cs
+++ PersonasBasicRest/Program.cs
@@ -1,13 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using PersonasBasicRest.Database;
 using PersonasBasicRest.Logger;
 
 var logger = LoggerUtils<Program>.GetLogger();
 
 var builder = WebApplication.CreateBuilder(args); // Crea una nueva instancia de WebApplicationBuilder con los argumentos de entrada
 
-// Add services to the container.
+// Añadimos los servicios necesarios para la aplicación web
+
+// Añadimos los contextos de la base de datos en memoria para pruebas
+builder.Services.AddDbContext<HeroDbContext>(options =>
+    {
+        options.UseInMemoryDatabase("Heroes")
+            .EnableSensitiveDataLogging();
+        logger.Debug("In-memory database added");
+    }
+);
+logger.Debug("Heroes in-memory database added");
+
+
+// Swagger/OpenAPI necesita estos servicios para funcionar
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer(); // Agrega el explorador de endpoints API para Swagger/OpenAPI
 builder.Services.AddSwaggerGen(); // Agrega SwaggerGen para generar documentación de la API
+logger.Debug("Swagger/OpenAPI services added");
+
+
+
+// Añade el middleware Swagger/OpenAPI a la aplicación web
 
 
 var app = builder.Build();  // Crea una nueva instancia de WebApplication con los servicios configurados
