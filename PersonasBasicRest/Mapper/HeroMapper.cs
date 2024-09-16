5c193369fa58a459c810d3fa257af97773435aeb
José Luis González <joseluisgonsan@gmail.com>
2024-09-16 10:40:35 +0200

    Mapper con Entity

---
+++ PersonasBasicRest/Mapper/HeroMapper.cs
@@ -0,0 +1,32 @@
+﻿using PersonasBasicRest.Database;
+using PersonasBasicRest.Models;
+
+namespace PersonasBasicRest.Mapper;
+
+/**
+ * Mapeador de objetos entre la capa de modelo y la capa de datos.
+ */
+public static class HeroMapper
+{
+    public static HeroEntity ToEntity(this Hero hero)
+    {
+        return new HeroEntity
+        {
+            Id = hero.Id,
+            Name = hero.Name,
+            IsBadGuy = hero.IsBadGuy
+        };
+    }
+
+    public static Hero ToModel(this HeroEntity heroEntity)
+    {
+        return new Hero
+        {
+            Id = heroEntity.Id,
+            Name = heroEntity.Name,
+            IsBadGuy = heroEntity.IsBadGuy,
+            CreatedAt = heroEntity.CreatedAt,
+            UpdatedAt = heroEntity.UpdatedAt
+        };
+    }
+}
