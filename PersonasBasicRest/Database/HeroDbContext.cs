11bd2d738fba0c3323411b63c78bef4864f3d774
José Luis González <joseluisgonsan@gmail.com>
2024-09-16 10:27:09 +0200

    Database Context

--- PersonasBasicRest/Database/HeroDbContext.cs
+++ PersonasBasicRest/Database/HeroDbContext.cs
@@ -9,5 +9,21 @@ namespace PersonasBasicRest.Database;
 public class HeroDbContext(DbContextOptions<HeroDbContext> options)
     : DbContext(options) // Hereda de DbContext para establecer la conexión con la base de datos. 
 {
-    public DbSet<HeroEntity> Heroes { get; set; } // Tabla de héroes en la base de datos del modelo
+    public DbSet<HeroEntity> Heroes { get; set; } // Tabla de héroes en la base de datos del modelo HeroEntity y la base de datos, nos da las consultas
+    
+    // En el método OnModelCreating se definen las relaciones entre las entidades y las tablas de la base de datos.
+    // Tambien se puede hacer por medio de DataAnnotations en las entidades.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Se añade el DbSet para la entidad en la base de datos
+        // Lo hace todo con las anotaciones de la entidad, así que solo voy a añadir las fechas
+        // Que son las que no se pueden hacer con anotaciones
+        modelBuilder.Entity<HeroEntity>(entity =>
+        {
+            entity.Property(e => e.CreatedAt).IsRequired()
+                .ValueGeneratedOnAdd(); // Se define que el campo CreatedAt es obligatorio y se genera automáticamente al añadir un registro
+            entity.Property(e => e.UpdatedAt).IsRequired()
+                .ValueGeneratedOnUpdate(); // Se define que el campo UpdatedAt es obligatorio y se genera automáticamente al actualizar un registro
+        });
+    }
 }
--- PersonasBasicRest/Database/HeroEntity.cs
+++ PersonasBasicRest/Database/HeroEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PersonasBasicRest.Database;
@@ -12,8 +13,15 @@ public class HeroEntity
     [Key] // Atributo que identifica cada héroe como clave primaria
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Autoincrementa el valor de Id cada vez que se crea un nuevo héroe
     public int Id { get; set; }
-    public string Name { get; set; }
-    public bool IsBadGuy { get; set; }
+    
+    [Required] // Atributo que indica que el nombre es requerido
+    [MaxLength(100)] // Atributo que limita la longitud del nombre a 100 caracteres
+    public string Name { get; set; } = string.Empty;
+
+    [Required] // Atributo que indica que la descripción es requerida
+    [DefaultValue(false)]
+    public bool IsBadGuy { get; set; } = false; // Atributo que indica si el héroe es malvado o no
+
     public DateTime CreatedAt { get; set; } = DateTime.Now; // Agrega la fecha de creación del héroe al crearlo
     public DateTime UpdatedAt { get; set; } = DateTime.Now; // Agrega la fecha de actualización del héroe al modificarlo
 }
